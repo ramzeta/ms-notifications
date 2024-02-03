1a236637340700ab7628d56b3ba4896b8c406233
ramzeta <ramiroperezsanz13@gmail.com>
2024-02-03 19:33:38 +0100

    first commit

---
+++ src/test/java/com/notifications/msnotifications/MsNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifications.msnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
